66984e7a3a2fbd52440c02fe4ccac49ba2f958b2
Russ Brooks <me@russbrooks.com>
2023-05-17 17:44:32 -0400

    different mail lib

--- routes/api/mail.ts
+++ routes/api/mail.ts
@@ -1,54 +1,84 @@
 import { Handlers, Status } from "$fresh/server.ts";
-import { SMTPClient } from "smtp";
+import { SmtpClient } from "smtp";
 import { generate } from "../../utils/generate.ts";
 
 export const handler: Handlers = {
   async POST(request: Request) {
-    const { SMTP_HOST, SMTP_PORT, SMTP_UN, SMTP_PW, SMTP_FROM, SMTP_TO } = Deno
-      .env.toObject();
-    console.log("*** SMTP_HOST: " + SMTP_HOST);
-    console.log("*** SMTP_PORT: " + SMTP_PORT);
-    console.log("*** SMTP_UN: " + SMTP_UN);
-    console.log("*** SMTP_PW: " + SMTP_PW);
-    console.log("*** SMTP_FROM: " + SMTP_FROM);
-    console.log("*** SMTP_TO: " + SMTP_TO);
+    // console.log(request);
 
-    console.log("*** SMTP_HOST: " + Deno.env.get("SMTP_HOST"));
-    console.log("*** SMTP_PORT: " + Deno.env.get("SMTP_PORT"));
-    console.log("*** SMTP_UN: " + Deno.env.get("SMTP_UN"));
-    console.log("*** SMTP_PW: " + Deno.env.get("SMTP_PW"));
-    console.log("*** SMTP_FROM: " + Deno.env.get("SMTP_FROM"));
-    console.log("*** SMTP_TO: " + Deno.env.get("SMTP_TO"));
+    const client = new SmtpClient();
 
-    const client = new SMTPClient({
-      connection: {
-        hostname: "smtp.gmail.com",
-        port: 587,
-        tls: true,
-        auth: {
-          username: "cbr1000@gmail.com",
-          password: "nhugedtzkvmrfcqr",
-        },
-      },
-    });
+    const connectConfig: any = {
+      hostname: Deno.env.get("SMTP_HOST")!,
+      port: +Deno.env.get("SMTP_PORT")!,
+      tls: true,
+      username: Deno.env.get("SMTP_UN")!,
+      password: Deno.env.get("SMTP_PW")!,
+    };
+    await client.connectTLS(connectConfig);
+
+    // await client.send({
+    //   from: Deno.env.get("SMTP_FROM"),
+    //   to: Deno.env.get("SMTP_TO"),
+    //   subject: "Welcome!",
+    //   content: "Hi from Vuelancer!",
+    // });
+
+    // await client.close();
+
+    // const res = await;
+    // await res.json();
 
     const payload: { mail: string; message: string } | undefined = await request
       .json();
     if (payload) {
-      try {
-        await client.send({
-          from: "cbr1000@gmail.com",
-          to: "me@russbrooks.com",
-          subject: `${payload.mail} sent a message from russbrooks.com`,
-          content: payload.message,
-        });
-        await client.close();
-
-        return new Response("", { status: Status.OK });
-      } catch (e) {
-        return new Response("", { status: Status.BadRequest });
-      }
+      // try {
+      await client.send({
+        from: "cbr1000@gmail.com",
+        to: "me@russbrooks.com",
+        subject: "Foo",
+        content: payload.message,
+      });
     }
-    return new Response("", { status: Status.NoContent });
+
+    await client.close();
+    // } catch (e) {
+    // return new Response("", { status: Status.BadRequest });
+    // }
+    // }
+    return new Response("", { status: Status.OK });
+    // return new Response("", { status: Status.NoContent });
+
+    // const client = new SMTPClient({
+    //   connection: {
+    //     hostname: "smtp.gmail.com",
+    //     port: 587,
+    //     tls: true,
+    //     auth: {
+    //       username: "cbr1000@gmail.com",
+    //       password: "nhugedtzkvmrfcqr",
+    //     },
+    //   },
+    // });
+
+    //     const payload: { mail: string; message: string } | undefined = await request
+    //       .json();
+    //     if (payload) {
+    //       try {
+    //         await client.send({
+    //           from: "cbr1000@gmail.com",
+    //           to: "me@russbrooks.com",
+    //           subject: `${payload.mail} sent a message from russbrooks.com`,
+    //           content: payload.message,
+    //         });
+    //         await client.close();
+    //
+    //         return new Response("", { status: Status.OK });
+    //       } catch (e) {
+    //         return new Response("", { status: Status.BadRequest });
+    //       }
+    //     }
+    //     return new Response("", { status: Status.NoContent });
+    // return new Response("", { status: Status.OK });
   },
 };
